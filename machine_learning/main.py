99a47ef1218c6f57c196d904ad2cca81c358fe2f
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2024-04-03 22:05:38 +0300

    whatever

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -5,15 +5,8 @@ import csv
 if __name__ == "__main__":
     # load the dataset.txt
     with open("dataset.txt", "r") as file:
-        dataset = file.read()
-
-    lines = dataset.split("\n")
-    columns = []
-    for line in lines:
-        column = re.split(r"\s+", line)
-        columns.append(column)
-    columns = columns[:-1]
+        lines = file.readlines()
 
     with open("dataset.csv", "w") as file:
-        writer = csv.writer(file)
-        writer.writerows(columns)
+        for line in lines:
+            file.write(",".join(line.split()) + "\n")
